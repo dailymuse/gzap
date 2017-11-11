cabeb8905c6313668b86ab3e38c914dab748c6f8
Dan Cadden <Shikkic@gmail.com>
2017-11-10 20:10:15 -0500

    change exported vars to functions

--- zap.go
+++ zap.go
@@ -1,6 +1,12 @@
 package gml
 
-import "go.uber.org/zap"
+import (
+	"fmt"
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
 
 // Any takes a key and an arbitrary value and chooses the best way to represent
 // them as a field, falling back to a reflection-based approach only if
@@ -9,109 +15,167 @@ import "go.uber.org/zap"
 // Since byte/uint8 and rune/int32 are aliases, Any can't differentiate between
 // them. To minimize surprises, []byte values are treated as binary blobs, byte
 // values are treated as uint8, and runes are always treated as integers.
-var Any = zap.Any
+func Any(key string, value interface{}) zapcore.Field {
+	return zap.Any(key, value)
+}
 
 // Array constructs a field with the given key and ArrayMarshaler. It provides
 // a flexible, but still type-safe and efficient, way to add array-like types
 // to the logging context. The struct's MarshalLogArray method is called lazily.
-var Array = zap.Array
+func Array(key string, val zapcore.ArrayMarshaler) zapcore.Field {
+	return zap.Array(key, val)
+}
 
 // Binary constructs a field that carries an opaque binary blob.
 //
 // Binary data is serialized in an encoding-appropriate format. For example,
 // zap's JSON encoder base64-encodes binary blobs. To log UTF-8 encoded text,
 // use ByteString.
-var Binary = zap.Binary
+func Binary(key string, val []byte) zapcore.Field {
+	return zap.Binary(key, val)
+}
 
 // Bool constructs a field that carries a bool.
-var Bool = zap.Bool
+func Bool(key string, val bool) zapcore.Field {
+	return zap.Bool(key, val)
+}
 
 // Bools constructs a field that carries a slice of bools.
-var Bools = zap.Bools
+func Bools(key string, bs []bool) zapcore.Field {
+	return zap.Bools(key, bs)
+}
 
 // ByteString constructs a field that carries UTF-8 encoded text as a []byte.
 // To log opaque binary blobs (which aren't necessarily valid UTF-8), use
 // Binary.
-var ByteString = zap.ByteString
+func ByteString(key string, val []byte) zapcore.Field {
+	return zap.ByteString(key, val)
+}
 
 // ByteStrings constructs a field that carries a slice of []byte, each of which
 // must be UTF-8 encoded text.
-var ByteStrings = zap.ByteStrings
+func ByteStrings(key string, bss [][]byte) zapcore.Field {
+	return zap.ByteStrings(key, bss)
+}
 
 // Complex128 constructs a field that carries a complex number. Unlike most
 // numeric fields, this costs an allocation (to convert the complex128 to
 // interface{}).
-var Complex128 = zap.Complex128
+func Complex128(key string, val complex128) zapcore.Field {
+	return zap.Complex128(key, val)
+}
 
 // Complex128s constructs a field that carries a slice of complex numbers.
-var Complex128s = zap.Complex128s
+func Complex128s(key string, nums []complex128) zapcore.Field {
+	return zap.Complex128s(key, nums)
+}
 
 // Complex64 constructs a field that carries a complex number. Unlike most
 // numeric fields, this costs an allocation (to convert the complex64 to
 // interface{}).
-var Complex64 = zap.Complex64
+func Complex64(key string, val complex64) zapcore.Field {
+	return zap.Complex64(key, val)
+}
 
 // Complex64s constructs a field that carries a slice of complex numbers.
-var Complex64s = zap.Complex64s
+func Complex64s(key string, nums []complex64) zapcore.Field {
+	return zap.Complex64s(key, nums)
+}
 
 // Duration constructs a field with the given key and value. The encoder
 // controls how the duration is serialized.
-var Duration = zap.Duration
+func Duration(key string, val time.Duration) zapcore.Field {
+	return zap.Duration(key, val)
+}
 
 // Durations constructs a field that carries a slice of time.Durations.
-var Durations = zap.Durations
+func Durations(key string, ds []time.Duration) zapcore.Field {
+	return zap.Durations(key, ds)
+}
 
 // Error is shorthand for the common idiom NamedError("error", err).
-var Error = zap.Error
+func Error(err error) zapcore.Field {
+	return zap.Error(err)
+}
 
 // Errors constructs a field that carries a slice of errors.
-var Errors = zap.Errors
+func Errors(key string, errs []error) zapcore.Field {
+	return zap.Errors(key, errs)
+}
 
 // Float32 constructs a field that carries a float32. The way the
 // floating-point value is represented is encoder-dependent, so marshaling is
 // necessarily lazy.
-var Float32 = zap.Float32
+func Float32(key string, val float32) zapcore.Field {
+	return zap.Float32(key, val)
+}
 
 // Float32s constructs a field that carries a slice of floats.
-var Float32s = zap.Float32s
+func Float32s(key string, nums []float32) zapcore.Field {
+	return zap.Float32s(key, nums)
+}
 
 // Float64 constructs a field that carries a float64. The way the
 // floating-point value is represented is encoder-dependent, so marshaling is
 // necessarily lazy.
-var Float64 = zap.Float64
+func Float64(key string, val float64) zapcore.Field {
+	return zap.Float64(key, val)
+}
 
 // Float64s constructs a field that carries a slice of floats.
-var Float64s = zap.Float64s
+func Float64s(key string, nums []float64) zapcore.Field {
+	return zap.Float64s(key, nums)
+}
 
 // Int constructs a field with the given key and value.
-var Int = zap.Int
+func Int(key string, val int) zapcore.Field {
+	return zap.Int(key, val)
+}
 
 // Int16 constructs a field with the given key and value.
-var Int16 = zap.Int16
+func Int16(key string, val int16) zapcore.Field {
+	return zap.Int16(key, val)
+}
 
 // Int16s constructs a field that carries a slice of integers.
-var Int16s = zap.Int16s
+func Int16s(key string, nums []int16) zapcore.Field {
+	return zap.Int16s(key, nums)
+}
 
 // Int32 constructs a field with the given key and value.
-var Int32 = zap.Int32
+func Int32(key string, val int32) zapcore.Field {
+	return zap.Int32(key, val)
+}
 
 // Int32s constructs a field that carries a slice of integers.
-var Int32s = zap.Int32s
+func Int32s(key string, nums []int32) zapcore.Field {
+	return zap.Int32s(key, nums)
+}
 
 // Int64 constructs a field with the given key and value.
-var Int64 = zap.Int64
+func Int64(key string, val int64) zapcore.Field {
+	return zap.Int64(key, val)
+}
 
 // Int64s constructs a field that carries a slice of integers.
-var Int64s = zap.Int64s
+func Int64s(key string, nums []int64) zapcore.Field {
+	return zap.Int64s(key, nums)
+}
 
 // Int8 constructs a field with the given key and value.
-var Int8 = zap.Int8
+func Int8(key string, val int8) zapcore.Field {
+	return zap.Int8(key, val)
+}
 
 // Int8s constructs a field that carries a slice of integers.
-var Int8s = zap.Int8s
+func Int8s(key string, nums []int8) zapcore.Field {
+	return zap.Int8s(key, nums)
+}
 
 // Ints constructs a field that carries a slice of integers.
-var Ints = zap.Ints
+func Ints(key string, nums []int) zapcore.Field {
+	return zap.Ints(key, nums)
+}
 
 // NamedError constructs a field that lazily stores err.Error() under the
 // provided key. Errors which also implement fmt.Formatter (like those produced
@@ -120,20 +184,26 @@ var Ints = zap.Ints
 //
 // For the common case in which the key is simply "error", the Error function
 // is shorter and less repetitive.
-var NamedError = zap.NamedError
+func NamedError(key string, err error) zapcore.Field {
+	return zap.NamedError(key, err)
+}
 
 // Namespace creates a named, isolated scope within the logger's context. All
 // subsequent fields will be added to the new namespace.
 //
 // This helps prevent key collisions when injecting loggers into sub-components
 // or third-party libraries.
-var Namespace = zap.Namespace
+func Namespace(key string) zapcore.Field {
+	return zap.Namespace(key)
+}
 
 // Object constructs a field with the given key and ObjectMarshaler. It
 // provides a flexible, but still type-safe and efficient, way to add map- or
 // struct-like user-defined types to the logging context. The struct's
 // MarshalLogObject method is called lazily.
-var Object = zap.Object
+func Object(key string, val zapcore.ObjectMarshaler) zapcore.Field {
+	return zap.Object(key, val)
+}
 
 // Reflect constructs a field with the given key and an arbitrary object. It uses
 // an encoding-appropriate, reflection-based function to lazily serialize nearly
@@ -142,67 +212,107 @@ var Object = zap.Object
 //
 // If encoding fails (e.g., trying to serialize a map[int]string to JSON), Reflect
 // includes the error message in the final log output.
-var Reflect = zap.Reflect
+func Reflect(key string, val interface{}) zapcore.Field {
+	return zap.Reflect(key, val)
+}
 
 // Skip constructs a no-op field, which is often useful when handling invalid
 // inputs in other Field constructors.
-var Skip = zap.Skip
+func Skip() zapcore.Field {
+	return zap.Skip()
+}
 
 // Stack constructs a field that stores a stacktrace of the current goroutine
 // under provided key. Keep in mind that taking a stacktrace is eager and
 // expensive (relatively speaking); this function both makes an allocation and
 // takes about two microseconds.
-var Stack = zap.Stack
+func Stack(key string) zapcore.Field {
+	return zap.Stack(key)
+}
 
 // String constructs a field with the given key and value.
-var String = zap.String
+func String(key string, val string) zapcore.Field {
+	return zap.String(key, val)
+}
 
 // Stringer constructs a field with the given key and the output of the value's
 // String method. The Stringer's String method is called lazily.
-var Stringer = zap.Stringer
+func Stringer(key string, val fmt.Stringer) zapcore.Field {
+	return zap.Stringer(key, val)
+}
 
 // Strings constructs a field that carries a slice of strings.
-var Strings = zap.Strings
+func Strings(key string, ss []string) zapcore.Field {
+	return zap.Strings(key, ss)
+}
 
 // Time constructs a zapcore.Field with the given key and value. The encoder
 // controls how the time is serialized.
-var Time = zap.Time
+func Time(key string, val time.Time) zapcore.Field {
+	return zap.Time(key, val)
+}
 
 // Times constructs a field that carries a slice of time.Times.
-var Times = zap.Times
+func Times(key string, ts []time.Time) zapcore.Field {
+	return zap.Times(key, ts)
+}
 
 // Uint constructs a field with the given key and value.
-var Uint = zap.Uint
+func Uint(key string, val uint) zapcore.Field {
+	return zap.Uint(key, val)
+}
 
 // Uint16 constructs a field with the given key and value.
-var Uint16 = zap.Uint16
+func Uint16(key string, val uint16) zapcore.Field {
+	return zap.Uint16(key, val)
+}
 
 // Uint16s constructs a field that carries a slice of unsigned integers.
-var Uint16s = zap.Uint16s
+func Uint16s(key string, nums []uint16) zapcore.Field {
+	return zap.Uint16s(key, nums)
+}
 
 // Uint32 constructs a field with the given key and value.
-var Uint32 = zap.Uint32
+func Uint32(key string, val uint32) zapcore.Field {
+	return zap.Uint32(key, val)
+}
 
 // Uint32s constructs a field that carries a slice of unsigned integers.
-var Uint32s = zap.Uint32s
+func Uint32s(key string, nums []uint32) zapcore.Field {
+	return zap.Uint32s(key, nums)
+}
 
 // Uint64 constructs a field with the given key and value.
-var Uint64 = zap.Uint64
+func Uint64(key string, val uint64) zapcore.Field {
+	return zap.Uint64(key, val)
+}
 
 // Uint64s constructs a field that carries a slice of unsigned integers.
-var Uint64s = zap.Uint64s
+func Uint64s(key string, nums []uint64) zapcore.Field {
+	return zap.Uint64s(key, nums)
+}
 
 // Uint8 constructs a field with the given key and value.
-var Uint8 = zap.Uint8
+func Uint8(key string, val uint8) zapcore.Field {
+	return zap.Uint8(key, val)
+}
 
 // Uint8s constructs a field that carries a slice of unsigned integers.
-var Uint8s = zap.Uint8s
+func Uint8s(key string, nums []uint8) zapcore.Field {
+	return zap.Uint8s(key, nums)
+}
 
 // Uintptr constructs a field with the given key and value.
-var Uintptr = zap.Uintptr
+func Uintptr(key string, val uintptr) zapcore.Field {
+	return zap.Uintptr(key, val)
+}
 
 // Uintptrs constructs a field that carries a slice of pointer addresses.
-var Uintptrs = zap.Uintptrs
+func Uintptrs(key string, us []uintptr) zapcore.Field {
+	return zap.Uintptrs(key, us)
+}
 
 // Uints constructs a field that carries a slice of unsigned integers.
-var Uints = zap.Uints
+func Uints(key string, us []uint) zapcore.Field {
+	return zap.Uints(key, us)
+}
